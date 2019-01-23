6e0366b5096d9595936486c99b07183bb4b83821
Igor Dereviankin <Igor_Dereviankin@epam.com>
2019-01-23 15:59:03 +0200

    init

---
+++ src/main/java/Consumer.java
@@ -0,0 +1,37 @@
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Properties;
+
+public class Consumer {
+
+    public static void main(String[] args) {
+
+        Properties consumerConfig = new Properties();
+        consumerConfig.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "sandbox-hdp.hortonworks.com:6667");
+        consumerConfig.put(ConsumerConfig.GROUP_ID_CONFIG, "my-group");
+        consumerConfig.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        consumerConfig.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        consumerConfig.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<byte[], byte[]> consumer = new KafkaConsumer<>(consumerConfig);
+
+        consumer.subscribe(Collections.singletonList("test"));
+
+        while (true) {
+            ConsumerRecords<byte[], byte[]> records = consumer.poll(1000);
+            for (ConsumerRecord<byte[], byte[]> record : records) {
+                System.out.printf("Received Message topic =%s, partition =%s, offset = %d, key = %s, value = %s\n", record.topic(), record.partition(), record.offset(), record.key(), record.value());
+            }
+
+            consumer.commitSync();
+        }
+
+    }
+
+}
