68db76662479c4aa44413446c831d9d6c04dcb96
Igor Dereviankin <Igor_Dereviankin@epam.com>
2019-01-29 15:35:40 +0200

    new mod

---
+++ src/main/java/New.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import org.apache.spark.SparkConf;
+import org.apache.spark.TaskContext;
+import org.apache.spark.api.java.*;
+import org.apache.spark.api.java.function.*;
+import org.apache.spark.streaming.Duration;
+import org.apache.spark.streaming.api.java.*;
+import org.apache.spark.streaming.kafka010.*;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import scala.Tuple2;
+
+public class New {
+
+    public static void main(String[] args) {
+        System.out.println("New!");
+
+        SparkConf conf = new SparkConf().setAppName("Streaming App").setMaster("local[*]");
+        JavaStreamingContext streamingContext = new JavaStreamingContext(conf, new Duration(1000));
+
+        Map<String, Object> kafkaParams = new HashMap<>();
+        kafkaParams.put("bootstrap.servers", "sandbox-hdp.hortonworks.com:6667");
+        kafkaParams.put("key.deserializer", StringDeserializer.class);
+        kafkaParams.put("value.deserializer", StringDeserializer.class);
+        kafkaParams.put("group.id", "1");
+        kafkaParams.put("auto.offset.reset", "latest");
+        kafkaParams.put("enable.auto.commit", false);
+
+        Collection<String> topic = Collections.singletonList("some");
+
+        JavaInputDStream<ConsumerRecord<String, String>> stream =
+                KafkaUtils.createDirectStream(
+                        streamingContext,
+                        LocationStrategies.PreferConsistent(),
+                        ConsumerStrategies.<String, String>Subscribe(topic, kafkaParams)
+                );
+
+        stream.mapToPair(record -> new Tuple2<>(record.key(), record.value())).print();
+
+    }
+
+}
