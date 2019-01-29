5f6a6a6fe26e2d28d1ac3634dbff69863d4bbd9d
Igor Dereviankin <Igor_Dereviankin@epam.com>
2019-01-29 16:19:10 +0200

    mod

--- src/main/java/New.java
+++ src/main/java/New.java
@@ -26,8 +26,8 @@ public class New {
         kafkaParams.put("value.deserializer", StringDeserializer.class);
         kafkaParams.put("group.id", "stream-hw");
         kafkaParams.put("kafka.consumer.id", "kafka-consumer-01");
-        //kafkaParams.put("auto.offset.reset", "latest");
-        //kafkaParams.put("enable.auto.commit", false);
+        kafkaParams.put("auto.offset.reset", "latest");
+        kafkaParams.put("enable.auto.commit", true);
 
         Collection<String> topic = Collections.singletonList("some");
 
