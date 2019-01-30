687e41f4d7d1bc2e2471fff648b6cbb681f41cf3
Igor Dereviankin <Igor_Dereviankin@epam.com>
2019-01-30 16:06:07 +0200

    next*** mod

--- src/main/java/New.java
+++ src/main/java/New.java
@@ -10,16 +10,14 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import scala.Tuple2;
-//import kafka.javaapi.consumer.SimpleConsumer;
 
 public class New {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("New* mod");
+        System.out.println("New*** mod");
 
         SparkConf conf = new SparkConf().setAppName("Streaming Homework").setMaster("local[*]");
-        JavaSparkContext sparkContext = new JavaSparkContext(conf);
-        JavaStreamingContext streamingContext = new JavaStreamingContext(sparkContext, new Duration(1000));
+        JavaStreamingContext streamingContext = new JavaStreamingContext(conf, new Duration(3000));
 
         Map<String, Object> kafkaParams = new HashMap<>();
         kafkaParams.put("bootstrap.servers", "sandbox-hdp.hortonworks.com:6667");
@@ -27,64 +25,26 @@ public class New {
         kafkaParams.put("value.deserializer", StringDeserializer.class);
         kafkaParams.put("group.id", "stream-hw");
         kafkaParams.put("kafka.consumer.id", "kafka-consumer-01");
-//        kafkaParams.put("auto.offset.reset", "latest");
-  //      kafkaParams.put("enable.auto.commit", false);
+        kafkaParams.put("auto.offset.reset", "latest");
+        kafkaParams.put("enable.auto.commit", false);
 
-
-        //WORKING!
-        /*
-        OffsetRange[] offsetRanges = {
-                OffsetRange.create("some", 0, 300, 400)
-        };
-        JavaRDD<ConsumerRecord<String, String>> rdd = KafkaUtils.createRDD(
-                sparkContext,
-                kafkaParams,
-                offsetRanges,
-                LocationStrategies.PreferConsistent()
-        );
-        rdd.map(ConsumerRecord::value).collect().forEach(System.out::println);
-*/
         Map<TopicPartition, Long> fromOffsets = new HashMap<>();
-        fromOffsets.put(new TopicPartition("some",0),0L);
-        //KafkaUtils.createDirectStream()
+        fromOffsets.put(new TopicPartition(args[0],0),0L);
 
-        Collection<String> topic = Collections.singletonList("some");
+        Collection<String> topic = Collections.singletonList(args[0]);
         JavaInputDStream<ConsumerRecord<String, String>> stream =
                 KafkaUtils.createDirectStream(
                         streamingContext,
                         LocationStrategies.PreferConsistent(),
                         ConsumerStrategies.<String, String>Subscribe(topic, kafkaParams, fromOffsets)
                 );
+        stream.print();
         //stream.mapToPair(record -> new Tuple2<>(record.key(), record.value()));
+        /*
         stream.foreachRDD(rdd -> {
             rdd.map(ConsumerRecord::value).collect().forEach(System.out::println);
         });
-
-  //      stream.foreachRDD(rdd -> {
-            //rdd.map(ConsumerRecord::value).collect().forEach(System.out::println); //!
-    //        OffsetRange[] offsetRanges = ((HasOffsetRanges) rdd.rdd()).offsetRanges();
-            //System.out.print("RDD COUNT: ");
-            //System.out.println( rdd.rdd().count() );
-
-//            System.out.println( "Cons record len: "+ rdd.rdd().collect().length );
-/*
-            for (int i=0; i<offsetRanges.length; i++) {
-                System.out.println( "Count "+offsetRanges[i].count() );
-                System.out.println( "From "+offsetRanges[i].fromOffset() );
-                System.out.println( "Part "+offsetRanges[i].partition() );
-                System.out.println( "Topic "+offsetRanges[i].topic() );
-                System.out.println( "Until "+offsetRanges[i].untilOffset() );
-            }
-*/
-            /*
-            rdd.foreachPartition(consumerRecords -> {
-                OffsetRange o = offsetRanges[TaskContext.get().partitionId()];
-                System.out.println(
-                        o.topic() + " " + o.partition() + " " + o.fromOffset() + " " + o.untilOffset());
-            });
-            */
-//        });
-
+        */
         streamingContext.start();
         streamingContext.awaitTermination();
 
